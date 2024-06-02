dc96425c279f66c8633cbf75ad86d56cc0ae271a
Davida Berto <davidabertho.93@gmail.com>
2024-06-02 15:03:38 +0100

    numpy import option done

---
+++ src/002.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Difficulty Level: L1
+# Q. Create a 1D array of numbers from 0 to 9
+# Desired output: array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+my_arr = np.arange(10)
+print(my_arr)
---
+++ src/003.py
@@ -0,0 +1,3 @@
+import numpy as np 
+# Difficulty Level: L1
+# Q. Create a 3×3 numpy array of all True’s
