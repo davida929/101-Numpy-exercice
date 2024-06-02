02a480f77afce4b35a280e2a1266036b30432cb7
Davida Berto <davidabertho.93@gmail.com>
2024-06-02 14:57:47 +0100

    exercice à1 done

---
+++ src/001.py
@@ -0,0 +1,10 @@
+# Difficulty Level: L1
+# Q. Import numpy as np and print the version number.
+
+import numpy as np
+
+# my solution 
+print("Full version : ",np.version.full_version)
+
+# solution 
+print(np.__version__)
